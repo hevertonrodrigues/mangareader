07621996dee5444229ca6df858775d7fbdc8e4b9
Raul <raulodaguiri@gmail.com>
2014-05-13 00:17:38 -0300

    Set basic configuration for front-end and set unicorn server for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,16 +24,19 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
+
+  # Add components path
+  config.assets.paths << Rails.root.join('app', 'assets', 'components')
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
